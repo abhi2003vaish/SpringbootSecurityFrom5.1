9ef8ead3baf9dab3b692b2e1fb2ee18f7e048941
abhi2003vaish <abhivaish2003@gmail.com>
2026-04-16 03:14:52 +0530

    after 5.8 (Spring security exceptional handling)

--- src/main/java/com/codingshuttle/SecurityApp/SecurityApplication/filters/JwtAuthFilter.java
+++ src/main/java/com/codingshuttle/SecurityApp/SecurityApplication/filters/JwtAuthFilter.java
@@ -8,11 +8,13 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
@@ -23,30 +25,38 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserService userService;
 
+    @Autowired
+    private HandlerExceptionResolver handlerExceptionResolver;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        //String the value of the Header with the key Authorization
-        final String requestTokenHeader=request.getHeader("Authorization");
-        //in company standard the Jwt token are stored in this form "Bearer ahdhgdhbjfcnjnkl.dxjnhxk.dxhbxudjx"
-        //if the stored is null and not start with "Bearer " then skips the filter and goes to the next filter in the chain
-        if(requestTokenHeader == null || !requestTokenHeader.startsWith("Bearer ")){
-            filterChain.doFilter(request,response);
-            return;
-        }
-        //here we are just taking the token in the token variable bec we need jwt token to verify
-        //String token =  requestTokenHeader.replace("Bearer ","");
-        String token = requestTokenHeader.split("Bearer ")[1];
-        //now verify the jwt token and we get the userId so we need to store it too
-        Long userId=jwtService.getUserIdFromToken(token);
+        try{
+            //String the value of the Header with the key Authorization
+            final String requestTokenHeader=request.getHeader("Authorization");
+            //in company standard the Jwt token are stored in this form "Bearer ahdhgdhbjfcnjnkl.dxjnhxk.dxhbxudjx"
+            //if the stored is null and not start with "Bearer " then skips the filter and goes to the next filter in the chain
+            if(requestTokenHeader == null || !requestTokenHeader.startsWith("Bearer ")){
+                filterChain.doFilter(request,response);
+                return;
+            }
+            //here we are just taking the token in the token variable bec we need jwt token to verify
+            //String token =  requestTokenHeader.replace("Bearer ","");
+            String token = requestTokenHeader.split("Bearer ")[1];
+            //now verify the jwt token and we get the userId so we need to store it too
+            Long userId=jwtService.getUserIdFromToken(token);
 
-        if(userId!=null && SecurityContextHolder.getContext().getAuthentication()==null){
-            User user=userService.getUserById(userId);
-            UsernamePasswordAuthenticationToken authenticationToken=new UsernamePasswordAuthenticationToken(user,
-                    null, null);
-            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            if(userId!=null && SecurityContextHolder.getContext().getAuthentication()==null){
+                User user=userService.getUserById(userId);
+                UsernamePasswordAuthenticationToken authenticationToken=new UsernamePasswordAuthenticationToken(user,
+                        null, null);
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+            filterChain.doFilter(request,response);
+
+        }catch (Exception ex){
+            handlerExceptionResolver.resolveException(request, response, null, ex);
         }
-        filterChain.doFilter(request,response);
 
 
 
