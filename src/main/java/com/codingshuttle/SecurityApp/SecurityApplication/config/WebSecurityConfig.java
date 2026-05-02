35c58fbc039fe6781397777eb4c4ad8875e4d521
abhi2003vaish <abhivaish2003@gmail.com>
2026-05-02 16:41:14 +0530

    after 6.5 (Granular Authorization with Authorities and Permissions in spring security)

--- src/main/java/com/codingshuttle/SecurityApp/SecurityApplication/config/WebSecurityConfig.java
+++ src/main/java/com/codingshuttle/SecurityApp/SecurityApplication/config/WebSecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static com.codingshuttle.SecurityApp.SecurityApplication.entities.enums.Permission.*;
 import static com.codingshuttle.SecurityApp.SecurityApplication.entities.enums.Role.ADMIN;
 import static com.codingshuttle.SecurityApp.SecurityApplication.entities.enums.Role.CREATOR;
 
@@ -44,6 +45,10 @@ public class WebSecurityConfig {
                         .requestMatchers(publicRoutes).permitAll()
                         .requestMatchers(HttpMethod.GET,"/posts/**").permitAll()
                         .requestMatchers(HttpMethod.POST,"/posts/**").hasAnyRole(ADMIN.name(), CREATOR.name())
+                        .requestMatchers(HttpMethod.POST,"/posts/**").hasAuthority(POST_CREATE.name())
+                        .requestMatchers(HttpMethod.GET,"/posts/**").hasAuthority(POST_VIEW.name())
+                        .requestMatchers(HttpMethod.PUT,"/posts/**").hasAuthority(POST_UPDATE.name())
+                        .requestMatchers(HttpMethod.DELETE,"/posts/**").hasAuthority(POST_DELETE.name())
 //                        .requestMatchers("/posts/**").hasAnyRole("ADMIN")
                         .anyRequest().authenticated())
                 .csrf(csrfConfig->csrfConfig
--- src/main/java/com/codingshuttle/SecurityApp/SecurityApplication/entities/User.java
+++ src/main/java/com/codingshuttle/SecurityApp/SecurityApplication/entities/User.java
@@ -1,6 +1,8 @@
 package com.codingshuttle.SecurityApp.SecurityApplication.entities;
 
+import com.codingshuttle.SecurityApp.SecurityApplication.entities.enums.Permission;
 import com.codingshuttle.SecurityApp.SecurityApplication.entities.enums.Role;
+import com.codingshuttle.SecurityApp.SecurityApplication.utils.PermissionMapping;
 import jakarta.persistence.*;
 //import org.jspecify.annotations.Nullable;
 import lombok.*;
@@ -9,6 +11,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -43,12 +46,37 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
+//    @ElementCollection(fetch = FetchType.EAGER)
+//    @Enumerated(EnumType.STRING)
+//    private Set<Permission> permissions;
+
 //   this method covert enum type role into the spring security understandable format GrantedAuthority
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        Set<SimpleGrantedAuthority> authorities= roles.stream()
+//                .map(role -> new SimpleGrantedAuthority("ROLE_"+role.name()))
+//                .collect(Collectors.toSet());
+//
+////        earlier we were providing/assigning the authorities based on roles but now we want to assign
+////        permissions as well inside the authorites
+//        permissions.forEach(
+//                permission -> authorities.add(new SimpleGrantedAuthority(permission.name()))
+//        );
+//        return authorities;
+//    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return roles.stream()
-                .map(role -> new SimpleGrantedAuthority("ROLE_"+role.name()))
-                .collect(Collectors.toSet());
+        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+
+        roles.forEach(
+                role -> {
+                    Set<SimpleGrantedAuthority> permissions= PermissionMapping.getAuthoritiesForRole(role);
+                    authorities.addAll(permissions);
+                    authorities.add(new SimpleGrantedAuthority("ROLE_" + role.name()));
+                }
+        );
+        return authorities;
     }
 
     @Override
